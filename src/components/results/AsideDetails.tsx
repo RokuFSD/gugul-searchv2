7aef6907557ea5e99d622048e5fd2d5980348642
RokuFSD <emilianordx@gmail.com>
2022-12-14 19:52:12 -0300

    Process data on Aside

--- src/components/results/AsideDetails.tsx
+++ src/components/results/AsideDetails.tsx
@@ -6,14 +6,16 @@ type AsideDetailsProps = {
 
 function AsideDetails({ data }: AsideDetailsProps) {
   const { list } = data;
-  const details = Object.entries(list as { [key: string]: string });
+
+  const toMap = (list || data) as { [key: string]: string };
+  const details = Object.keys(toMap).filter((key) => (typeof toMap[key] === "string" || Array.isArray(toMap[key]) && !key.includes("link") && !key.includes("links")));
   return (
     <table className="table-auto my-2 max-w-md md:max-w-full w-full md:relative">
       <tbody>
       {details.map((detail, index) => (
-        <tr key={detail[0]} className={`flex justify-start ${index % 2 === 0 ? "bg-gray-600" : ""} md:px-10 lg:px-0`}>
-          <th className="basis-10/12 text-start">{detail[0]}</th>
-          <td>{detail[1]}</td>
+        <tr key={detail} className={`flex justify-start ${index % 2 === 0 ? "bg-gray-600" : ""} md:px-10 lg:px-4`}>
+          <th className="basis-7/12 text-start">{detail}</th>
+          <td className="basis-5/12 text-end">{toMap[detail]}</td>
         </tr>
       ))}
       </tbody>
--- src/components/results/AsideSection.tsx
+++ src/components/results/AsideSection.tsx
@@ -19,7 +19,8 @@ const stateMachine = {
 
 function AsideSection({ data }: AsideSectionProps) {
   const [dataState, setDataState] = useState<"closed" | "open">("closed");
-  const { header_images: headerImages, title, description, type, ...rest } = data;
+  const { header_images: headerImages, title, description, type, source, ...rest } = data;
+  const { link, name } = source as { link: string, name: string };
   return (
     <aside
       className="relative md:flex md:flex-wrap lg:block order-1 lg:order-2 shadow-xl lg:shadow-none rounded-lg p-2 md:w-full lg:w-auto">
@@ -27,8 +28,11 @@ function AsideSection({ data }: AsideSectionProps) {
       <div
         data-state={dataState}
         className="md:w-96 lg:w-auto lg:max-w-lg relative md:basis-1/2 md:mx-5 lg:mx-0">
-        <h1 className="text-2xl font-semibold xl:text-3xl">{title}</h1>
-        <p className="text-sm md:text-base xl:text-lg">{description}</p>
+        <a href={link} target="_blank" rel="noreferrer">
+          <h1 className="text-2xl font-semibold xl:text-3xl">{title}</h1>
+          <p className="text-sm md:text-base xl:text-lg">{description}</p>
+        </a>
+        <p className="italic text-gray-300">Source: {name}</p>
       </div>
       <AsideDetails data={rest} />
       <button className="w-full flex flex-col items-center md:top-full lg:hidden md:basis-full" type="button"
