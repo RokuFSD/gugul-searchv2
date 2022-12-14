d7e30574f1cbec97e86df1bb36f2dbd0f405a549
RokuFSD <emilianordx@gmail.com>
2022-12-14 03:09:47 -0300

    Added new card component for results

---
+++ src/components/cards/ResultCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type ResultCardProps = {
+  item: {
+    title: string;
+    displayed_link: string;
+    link: string;
+    thumbnail: string;
+    snippet: string;
+  }
+}
+
+function ResultCard({ item }: ResultCardProps) {
+  return (
+    <div className="flex flex w-full items-center gap-4">
+      <div className="w-full">
+        <span className="text-xs text-gray-400">{item.displayed_link}</span>
+        <h2 className="text-lg text-blue-300 hover:text-blue-400 transition-all">
+          <a href={item.link} target="_blank" rel="noreferrer">
+            {item.title}
+          </a>
+        </h2>
+        <p className="text-md overflow-hidden overflow-ellipsis line-clamp-2 lg:line-clamp-5">{item.snippet}</p>
+      </div>
+      {item.thumbnail ? (
+        <img src={item.thumbnail} alt={item.title} className="w-20 h-20 rounded-lg md:w-24 md:h-24" />) : null}
+    </div>
+  );
+}
+
+export default ResultCard
--- src/components/results/MainSection.tsx
+++ src/components/results/MainSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ResponseType } from "../../types/api";
+import ResultCard from "../cards/ResultCard";
 
 type MainSectionProps = {
   data: ResponseType["data"]["organic_results"];
@@ -9,19 +10,7 @@ function MainSection({ data }: MainSectionProps) {
   return (
     <div className="flex flex-col gap-10 w-full lg:max-w-xl">
       {data?.map((item, index) => (
-        <div key={item.link} className="flex flex w-full items-center gap-4">
-          <div className="w-full">
-            <span className="text-xs text-gray-400">{item.displayed_link}</span>
-            <h2 className="text-lg text-blue-300 hover:text-blue-400 transition-all">
-              <a href={item.link} target="_blank" rel="noreferrer">
-                {item.title}
-              </a>
-            </h2>
-            <p className="text-md overflow-hidden overflow-ellipsis line-clamp-2 lg:line-clamp-5">{item.snippet}</p>
-          </div>
-          {item.thumbnail ? (
-            <img src={item.thumbnail} alt={item.title} className="w-20 h-20 rounded-lg md:w-24 md:h-24" />) : null}
-        </div>
+        <ResultCard item={item} key={item.link} />
       ))}
     </div>
   );
