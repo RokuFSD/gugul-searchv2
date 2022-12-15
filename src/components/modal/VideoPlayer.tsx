16af91d183e09a2ce444a84a12b1638fc77d1379
RokuFSD <emilianordx@gmail.com>
2022-12-15 16:17:56 -0300

    Modal with tab trapping

--- src/components/modal/VideoPlayer.tsx
+++ src/components/modal/VideoPlayer.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback, useState } from "react";
-import Modal from "./Modal";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Modal from "react-modal";
 import Spinner from "../svgs/Spinner";
+
+Modal.setAppElement("#modal-root");
 
 function VideoPlayer({ src, thumbnail, title }: { src: string, thumbnail: string, title: string }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,22 +16,39 @@ function VideoPlayer({ src, thumbnail, title }: { src: string, thumbnail: string
     setIsLoading(false);
   }
 
+  const overlay = {
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+    zIndex: 1000
+  };
+
+  const content = {
+    backgroundColor: "transparent",
+    border: "none"
+  };
   return (
     <>
       <button onClick={() => setIsOpen(true)} type="button">
         <img src={thumbnail} alt={title}
              className="w-48 h-28 rounded-lg object-fit" />
       </button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        {isLoading && (
-          <div className="z-30 absolute">
-            <Spinner />
-          </div>
-        )}
-        <iframe className="z-40 w-[800px] h-[500px] rounded-lg"
-                src={srcEmbed}
-                title={src}
-                onLoad={handleLoad} />
+      <Modal isOpen={isOpen} onRequestClose={onClose} style={{ overlay, content }}>
+        <div className="flex flex-col gap-8 h-full items-center justify-center">
+          <button type="button" onClick={onClose} className="bg-white rounded-full p-2">
+            <svg className="w-6 h-6" viewBox="0 0 24 24">
+              <path fill="currentColor"
+                    d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+            </svg>
+          </button>
+          {isLoading && (
+            <div className="z-30 absolute">
+              <Spinner />
+            </div>
+          )}
+          <iframe className="z-40 w-[800px] h-[500px] rounded-lg"
+                  src={srcEmbed}
+                  title={src}
+                  onLoad={handleLoad} />
+        </div>
 
       </Modal>
     </>
