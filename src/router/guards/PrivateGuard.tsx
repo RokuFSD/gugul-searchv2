c0913143723a3364e31c153d4157f9e7ee1b889c
RokuFSD <emilianordx@gmail.com>
2023-01-01 17:10:17 -0300

    Add session check hook

--- src/router/guards/PrivateGuard.tsx
+++ src/router/guards/PrivateGuard.tsx
@@ -1,37 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { PublicRoutes } from "../../models/routes";
-import { useAppDispatch } from "../../redux/hooks/store";
-import { me } from "../../redux/features/auth/thunkActions";
+import { useAppSelector } from "../../redux/hooks/store";
 import { selectUser } from "../../redux/features/auth/authSlice";
 
 function PrivateGuard() {
-  const dispatch = useAppDispatch();
-  const user = useSelector(selectUser);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchUser() {
-      dispatch(me())
-        .unwrap()
-        .catch((err) => {})
-        .finally(() => setIsLoading(false));
-    }
-
-    if (!user.email) {
-      setIsLoading(true);
-      fetchUser();
-    }
-  }, []);
+  const user = useAppSelector(selectUser);
 
   if (!user.email) {
-    return isLoading ? (
-      <div>Loading...</div>
-    ) : (
-      <Navigate to={PublicRoutes.AUTH} />
-    );
+    return <Navigate to={PublicRoutes.AUTH} />;
   }
   return <Outlet />;
 }
