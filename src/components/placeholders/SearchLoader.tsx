71d3336eb84c0d36f3a712a83b0d7d5d03216a3f
RokuFSD <emilianordx@gmail.com>
2023-03-18 19:39:49 -0300

    Type on class

--- src/components/placeholders/SearchLoader.tsx
+++ src/components/placeholders/SearchLoader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SearchLoader() {
   return (
-    <div className="w-full flex top-0 absolute z-50 h-full items-center justify-center -z-50" aria-busy="true"
+    <div className="w-full flex top-0 absolute h-full items-center justify-center -z-50" aria-busy="true"
          aria-label="loader">
       <div className="lds-ripple">
         <div />
